e43522b4f8591e4866e11aada88d4b60a3282d3f
mohamedmenisy <mohamedosamamenisy@gmail.com>
2023-04-22 00:23:01 +0200

    WeatherApp

---
+++ JS/index.js
@@ -0,0 +1,67 @@
+let SearchButton=document.getElementById('SearchButton');
+let inputSearch=document.getElementById('inputSearch');
+let errorImg=document.querySelector('.errorImg');
+let temperaturebox=document.querySelector('.temperature');
+let weatherBox=document.getElementById('weather')
+SearchButton.addEventListener('click',()=>{
+    let city=inputSearch.value;
+
+   getdata(city)
+})
+
+async function getdata(city){
+    let respons=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=073a4320452fbdc1ad1aeaa181c57b51`);
+    let data=await respons.json();
+    console.log(data.cod);
+    if(data.cod==='404'){
+        weatherBox.style.height="350px"
+        temperaturebox.style.display='none'
+        setTimeout(()=>{
+            errorImg.style.display='block'
+        },500)
+    }else{
+        weatherBox.style.height="570px"
+        errorImg.style.display='none'
+        setTimeout(()=>{
+            temperaturebox.style.display='block'
+        },500)
+        let tempNumber=document.querySelector('.tempNumber');
+        let tempdesc=document.querySelector('.tempdesc');
+        let humidityNumber=document.querySelector('.humidityNumber');
+        let windSpeed=document.querySelector('.windSpeed');
+        let weatherimg=document.querySelector('.temperature img')
+
+        switch (data.weather[0].main) {
+            case 'Clear':
+                weatherimg.src='./images/clear.png'
+                break;
+        
+                case 'Clouds':
+                    weatherimg.src='./images/cloud.png'
+                    break;
+
+                     case 'Rain':
+                        weatherimg.src='./images/rain.png'
+                        break;
+                        case 'Snow':
+                            weatherimg.src='./images/rain.png'
+                            break;
+                            case 'Haze':
+                                weatherimg.src='./images/mist.png'
+                                break;
+                    default:
+                        weatherimg.src=''
+        }
+
+        console.log(data.weather[0]);
+
+
+        tempNumber.innerHTML=`${parseInt(data.main.temp)}<sup>°C</sup>`
+        tempdesc.innerHTML=`${data.weather[0].description}`
+        humidityNumber.innerHTML=`${data.main.humidity}%`
+        windSpeed.innerHTML=`${parseInt(data.wind.speed)}Km/h`
+
+    }
+   
+    
+}
