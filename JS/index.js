11352889f4b218ee0e968d32373d5210731eeae0
mohamedmenisy <mohamedosamamenisy@gmail.com>
2023-04-23 15:25:31 +0200

    update

--- JS/index.js
+++ JS/index.js
@@ -3,16 +3,22 @@ let inputSearch=document.getElementById('inputSearch');
 let errorImg=document.querySelector('.errorImg');
 let temperaturebox=document.querySelector('.temperature');
 let weatherBox=document.getElementById('weather')
+let spin=document.getElementById('spin')
+
 SearchButton.addEventListener('click',()=>{
     let city=inputSearch.value;
-
+    SearchButton.style.display='none'
+    spin.style.display='block'
    getdata(city)
 })
 
 async function getdata(city){
+    
     let respons=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=073a4320452fbdc1ad1aeaa181c57b51`);
     let data=await respons.json();
-    console.log(data.cod);
+    console.log(data.main);
+    SearchButton.style.display='block'
+    spin.style.display='none'
     if(data.cod==='404'){
         weatherBox.style.height="350px"
         temperaturebox.style.display='none'
@@ -52,10 +58,6 @@ async function getdata(city){
                     default:
                         weatherimg.src=''
         }
-
-        console.log(data.weather[0]);
-
-
         tempNumber.innerHTML=`${parseInt(data.main.temp)}<sup>°C</sup>`
         tempdesc.innerHTML=`${data.weather[0].description}`
         humidityNumber.innerHTML=`${data.main.humidity}%`
